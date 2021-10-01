0725b1a981fa44d6765340756c1a2d77e365e02e
Khanh Le <ngunhucutay@gmail.com>
2021-10-01 16:54:18 -0500

    image assets

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React from 'react';
 import type {Node} from 'react';
 import {
+  Dimensions,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -17,6 +18,7 @@ import {
   Image,
   useColorScheme,
   View,
+  ImageBackground,
 } from 'react-native';
 
 import {
@@ -28,6 +30,9 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import image1 from './assets/images/ph1.jpg'
+import image2 from './assets/images/ph2.jpg'
+import image3 from './assets/images/ph3.jpg'
+import chicagoImg from './assets/images/chicago-nightlife.jpeg'
 
 // import RootNavigator from './directory/TabNavigator/RootNavigator';
 import {NavigationContainer} from '@react-navigation/native';
@@ -39,38 +44,57 @@ import { MdExplore, MdHome } from 'react-icons/md'
 
 const BottomTab = createBottomTabNavigator();
 let isDarkMode;
-// const Section = ({children, title}): Node => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
+const win = Dimensions.get('window');
+const ratio = win.width / 541
 
 const BirthdayPack: () => Node = () => {
   return (
     <View style={styles.productOption}>
-      <Image style={styles.image1} source={image1} />
+        <ImageBackground style={styles.productImage} source={image1}>
+        <Text style={styles.productText}>
+          Birthday Package
+        </Text>
+      </ImageBackground>
     </View>
-  )
+  );
+}
+
+const BacheloretteParty: () => Node = () => {
+  return (
+    <View style={styles.productOption}>
+        <ImageBackground style={styles.productImage} source={image2}>
+        <Text style={styles.productText}>
+        Bachelorette Party
+        </Text>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const SurpriseParty: () => Node = () => {
+  return (
+    <View style={styles.productOption}>
+        <ImageBackground style={styles.productImage} source={image3}>
+        <Text style={styles.productText}>
+        Surprise Birthday Party
+        </Text>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const HomeScreenHeader: () => Node = () => {
+  return (
+    <View screenOptions={{
+      headerStyle: { position: 'absolute', top: 0, left: 0 },
+    }}>
+      <ImageBackground style={{width: "100%", height: 500, top: 0}} source={chicagoImg}>
+        <Text style={styles.headerText}>
+          Welcome To Chicago Nightlife Experience
+        </Text>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const App: () => Node = () => {
@@ -82,19 +106,23 @@ const App: () => Node = () => {
 
   return (
     <View style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar translucent backgroundColor='transparent' 
+                // barStyle={isDarkMode ? 'light-content' : 'dark-content'} 
+      />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={styles.contentContainer}
+        contentInsetAdjustmentBehavior="scrollableAxes"
         style={backgroundStyle}>
-        <Header />
-        {/* <View style={styles.container}>
-        </View> */}
+        {/* <Header /> */}
+        <HomeScreenHeader />
         <BirthdayPack />
+        <BacheloretteParty />
+        <SurpriseParty />
       </ScrollView>
       <NavigationContainer>
-        <BottomTab.Navigator 
+        <BottomTab.Navigator
           screenOptions={{
-            tabBarStyle: { position: 'absolute' },
+            tabBarStyle: { position: 'absolute', backgroundColor: {backgroundStyle} },
           }}>
               <BottomTab.Screen name="Home" component={Home} 
               option={{ tabBarIcon: MdHome}} />
@@ -106,34 +134,43 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
+  headerText: {
+    position: "absolute",
+    fontSize: 40,
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+    left: 2, right: 2, bottom: 20, 
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
   container: {
     flex: 1,
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     alignItems: "center"
   },
   productOption: {
-    width: 460,
-    height: 250
+    position: "relative",
+    alignItems: "center",
+    // paddingTop: 12,
+    paddingBottom: 12,
   },
-  image1: {
-    width: 300,
-    height: 150
+  productText: {
+    position: "absolute",
+    fontSize: 32,
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+    left: 16, right: 16, bottom: 2,
+    color: "#FFFFFF",
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  productImage: {
+    alignSelf: "stretch",
+    width: win.width,
+    height: 320 * ratio,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  contentContainer: {
+    paddingBottom: 24
   },
 });
 
